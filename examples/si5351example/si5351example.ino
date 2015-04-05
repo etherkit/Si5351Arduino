4e6f6c39d8e3cf52a9ab75c31ae0902f0daf4fbb
Jason Milldrum <milldrum@gmail.com>
2015-04-04 22:11:17 -0700

    Add si5351phase example

--- examples/si5351example/si5351example.ino
+++ examples/si5351example/si5351example.ino
@@ -1,22 +1,3 @@
-/*
- * si5351example.ino - Simple example of using the Si5351Arduino library
- *
- * Copyright (C) 2015 Jason Milldrum <milldrum@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "si5351.h"
 #include "Wire.h"
 
@@ -26,19 +7,19 @@ void setup()
 {
   // Start serial and initialize the Si5351
   Serial.begin(57600);
-  si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0);
+  si5351.init(SI5351_CRYSTAL_LOAD_8PF);
 
   // Set CLK0 to output 14 MHz with a fixed PLL frequency
   si5351.set_pll(SI5351_PLL_FIXED, SI5351_PLLA);
-  si5351.set_freq(1400000000ULL, SI5351_PLL_FIXED, SI5351_CLK0);
+  si5351.set_freq(14000000, SI5351_PLL_FIXED, SI5351_CLK0);
 
   // Set CLK1 to output 20 MHz
-  si5351.set_freq(2000000000ULL, 0ULL, SI5351_CLK1);
+  si5351.set_freq(20000000, 0, SI5351_CLK1);
 }
 
 void loop()
 {
-  // Read the Status Register and print it every 10 seconds
+  // Read the Status Register and print it every second
   si5351.update_status();
   Serial.print("SYS_INIT: ");
   Serial.print(si5351.dev_status.SYS_INIT);
@@ -51,5 +32,5 @@ void loop()
   Serial.print("  REVID: ");
   Serial.println(si5351.dev_status.REVID);
 
-  delay(10000);
+  delay(1000);
 }
